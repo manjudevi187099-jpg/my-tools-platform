c148e6036b37ad3aa323f8bd09e5d20a8112f401
MANJU DEVI <manjudevi187099@gmail.com>
2026-05-30 15:42:33 +0530

    feat: added invert-pdf tool to registry and router

--- src/tools/pdf-invert-colors/index.tsx
+++ src/tools/pdf-invert-colors/index.tsx
@@ -45,7 +45,7 @@ export default function InvertPdfColors() {
       }
 
       const pdfBytes = await newPdfDoc.save();
-      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const blob = new Blob([new Uint8Array(pdfBytes as any)], { type: 'application/pdf' });
       const url = URL.createObjectURL(blob);
       const a = document.createElement('a');
       a.href = url;
